52c867eb55c0ae7ee775676c2180f057ddb60a35
Ishini Kiridena <ishini.kiridenaedu@gmail.com>
2021-03-21 17:07:53 +0530

    User function layouts committed

---
+++ app/src/main/java/com/example/LivFit/DisplayBMI.java
@@ -0,0 +1,14 @@
+package com.example.LivFit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayBMI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_b_m_i);
+    }
+}
